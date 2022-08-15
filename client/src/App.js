47abe34ab9f7664ad06bac5aa802285f57acba82
maherabdoualy <gh2775@wayne.edu>
2022-08-14 22:19:42 -0400

    commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+
+const code = new URLSearchParams(window.location.search).get("code");
+
+function App() {
+  return code ? <Dashboard code={code} /> : <Login />;
+}
+
+export default App;
